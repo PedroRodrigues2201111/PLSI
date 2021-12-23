19ef9bb9313a5b659982f766216ae0aa3f5afaeb
Pedro Rodrigues <2201111@my.ipleiria.pt>
2021-12-23 14:00:15 +0000

    Corrigir os problemas relacionados com o bootstrap no frontend

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,24 +36,35 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        //['label' => 'About', 'url' => ['/site/about']],
+        //['label' => 'Contact', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
+      $menuItems[] = [
+        'label' => Yii::$app->user->identity->getDisplayName(),
+//            'dropDownOptions' => [
+//                'class' => 'dropdown-menu-right'
+//            ],
+        'items' => [
+          [
+            'label' => 'Profile',
+            'url' => ['/profile/index'],
+          ],
+          [
+            'label' => 'Logout',
+            'url' => ['/site/logout'],
+            'linkOptions' => [
+              'data-method' => 'post'
+            ],
+          ]
+        ]
+      ];
     }
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
+        'options' => ['class' => 'navbar-nav ml-auto'],
         'items' => $menuItems,
     ]);
     NavBar::end();
@@ -62,9 +73,6 @@ AppAsset::register($this);
 
 <main role="main" class="flex-shrink-0">
     <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
